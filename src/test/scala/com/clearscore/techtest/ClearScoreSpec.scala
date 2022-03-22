da5194fadafdc668061c40f873bd8d169252434c
Luke Martin <compsciguy396@gmail.com>
2022-03-22 21:22:25 +0000

    Start to do TDD with ScalaTest

--- src/test/scala/com/clearscore/techtest/ClearScoreSpec.scala
+++ src/test/scala/com/clearscore/techtest/ClearScoreSpec.scala
@@ -1,11 +1,23 @@
 package com.clearscore.techtest
 
+import com.clearscore.techtest.models._
 import org.scalatest.freespec.AnyFreeSpec
 
 class ClearScoreSpec extends AnyFreeSpec {
-  "A simple test" - {
-    "be true" in {
-      assert(true)
+
+  "Each credit card provider" - {
+    "CSCards can respond with data" in {
+      val mockUser = UserQueryFormat1("John Smith", 500)
+    }
+
+    "ScoredCards can respond with data" in {
+      val mockUser = UserQueryFormat2("John Smith", 341, 18500)
+    }
+  }
+
+  "Credit card query service" - {
+    "return a set of credit card offers" in {
+      val mockUser = User("John Smith", 500, 28000)
     }
   }
 }
