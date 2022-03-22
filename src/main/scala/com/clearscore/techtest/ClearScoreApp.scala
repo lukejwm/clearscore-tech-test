e2d26549a1f41855dec52695ee5e54e3bad7f8d7
Luke Martin <compsciguy396@gmail.com>
2022-03-22 19:37:32 +0000

    Checking that endpoints are working and returning data from scala app

--- src/main/scala/com/clearscore/techtest/ClearScoreApp.scala
+++ src/main/scala/com/clearscore/techtest/ClearScoreApp.scala
@@ -6,8 +6,13 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.unmarshalling._
 import com.clearscore.techtest.config.Config
 import com.clearscore.techtest.models._
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Success, Failure}
 
 object ClearScoreApp extends App
   with Config
@@ -19,11 +24,42 @@ object ClearScoreApp extends App
   implicit val actorSystem: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "ClearScoreApp")
   implicit val userMarshaller: spray.json.RootJsonFormat[User] = jsonFormat3(User.apply)
 
-  val route: Route = (path("creditcards") & post) {
-    entity(as[User]) { _ =>
-      complete(CardQueryServiceResponse("CardProvider", "ACard", 23.4, 0.6))
+  val csCardsEndpoint = config.getString("development.providers.url.cscards")
+  val scoredCardsEndpoint = config.getString("development.providers.url.scoredcards")
+
+  def csCardsRequest(): Future[HttpResponse] =
+    Http(actorSystem).singleRequest(
+      HttpRequest(
+        HttpMethods.POST,
+        csCardsEndpoint,
+        entity = HttpEntity(ContentTypes.`application/json`, "{\"name\": \"John Smith\", \"creditScore\": 500}")
+      )
+    )
+
+  def scoredCardsRequest() = {
+    val response = Http(actorSystem).singleRequest(
+      HttpRequest(
+        uri = scoredCardsEndpoint,
+        method = HttpMethods.POST,
+        entity = HttpEntity(ContentTypes.`application/json`, "{\"name\": \"John Smith\", \"score\": 500, \"salary\": 18500}")
+      )
+    )
+
+    response onComplete {
+      case Success(res) =>
+        val scoredCardsRes: Future[ScoredCardsResponse] = Unmarshal(res.entity).to[ScoredCardsResponse]
+        println(scoredCardsRes)
+      case Failure(es) => println("This Failed")
     }
   }
 
-  Http().newServerAt(host, port).bind(route)
+  scoredCardsRequest()
+
+//  val route: Route = (path("creditcards") & post) {
+//    entity(as[User]) { _ =>
+//      complete(CardQueryServiceResponse("CardProvider", "ACard", 23.4, 0.6))
+//    }
+//  }
+//
+//  Http().newServerAt(host, port).bind(route)
 }
