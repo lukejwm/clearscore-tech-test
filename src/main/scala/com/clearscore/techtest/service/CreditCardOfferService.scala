7aa217c813dca4e875b00b412225979b7660ae40
Luke Martin <compsciguy396@gmail.com>
2022-03-24 18:21:03 +0000

    Fixed connectivity issues with ScoredCards endpoint (bad url) and can retrieve data from both endpoints

--- src/main/scala/com/clearscore/techtest/service/CreditCardOfferService.scala
+++ src/main/scala/com/clearscore/techtest/service/CreditCardOfferService.scala
@@ -1,70 +1,94 @@
 package com.clearscore.techtest.service
 
-import akka.actor.typed.ActorSystem
+import akka.actor.typed.{ActorRef, ActorSystem}
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshalling.ToResponseMarshallable
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.client.RequestBuilding.Post
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import com.clearscore.techtest.ClearScoreApp.log
 import com.clearscore.techtest.config._
 import com.clearscore.techtest.models._
 import org.slf4j.LoggerFactory
 import spray.json._
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
 
 trait CreditCardOfferService extends ServiceJsonProtocol {
+  implicit val actorSystem = ActorSystem(Behaviors.empty, "ClearScoreApp")
+  implicit val userMarshaller: spray.json.RootJsonFormat[User] = jsonFormat3(User.apply)
 
-//  implicit val actorSystem = ActorSystem(Behaviors.empty, "ClearScoreApp")
-//  implicit val userMarshaller: spray.json.RootJsonFormat[User] = jsonFormat3(User.apply)
+  //for reading JSON responses
+  implicit val modelJsonReaderCsCards = new JsonReader[CSCardsResponse] {
+    override def read(json: JsValue): CSCardsResponse = ???
+  }
 
   val log = LoggerFactory.getLogger("om.clearscore.techtest.service.CreditCardOfferService")
 
-  def runService(host: String, port: Int, csCardsEndpoint: String, scoredCardsEndpoint: String) = {
+  def runService(host: String, port: Int, csCardsEndpoint: String, scoredCardsEndpoint: String): Future[Http.ServerBinding] = {
     log.info(s"Starting service on host: ${host}, port: ${port}")
     log.debug(s"CSCards Endpoint: ${csCardsEndpoint}")
     log.debug(s"ScoredCards Endpoint: ${scoredCardsEndpoint}")
+
+    val route: Route = (path("creditcards") & post) {
+      entity(as[User]) { user =>
+        log.info(s"Responding to userId: ${user.name}")
+        complete(findCreditCardDeals(user, csCardsEndpoint, scoredCardsEndpoint))
+      }
+    }
+
+    Http().newServerAt(host, port).bind(route)
   }
 
-//  def fetchCsCardOffers(user: User, endpoint: String)(implicit executionContext: ExecutionContext): Future[CSCardsResponse] = {
-//    val body = s"""{"name": "${user.name}", "creditScore": ${user.creditScore}}"""
-//
-//    val csCardOffers = Http().singleRequest(HttpRequest(method = HttpMethods.POST, uri = endpoint,
-//      entity = HttpEntity(ContentTypes.`application/json`, body))).flatMap {
-//      case HttpResponse(StatusCodes.OK, _, entity, _) =>
-//        entity.toStrict(5.seconds).map(strictEntity => JsonParser(ParserInput(strictEntity.data.utf8String)))
-//          .map(_.convertTo[CSCardsResponse])
-//
-//      case HttpResponse(status, _, entity, _) =>
-//        entity.toStrict(5.seconds).map(_.data.utf8String).flatMap(resultString => Future.failed(new IllegalStateException(
-//          s"Failed to get credit card offers from CSCards. URI: ${endpoint}, HTTP Status ${status}, entity: ${resultString}"
-//        )))
-//    }
-//
-//    //TODO: ensure this works and use Success/Failure cases for this
-//    csCardOffers.onComplete(res => res)
-//    csCardOffers
-//  }
-//
-//  def fetchScoredCardsOffers(user: User, endpoint: String)(implicit executionContext: ExecutionContext): Future[ScoredCardsResponse] = {
-//    val body = s"""{"name": "${user.name}, "score": ${user.creditScore}, "salary": ${user.salary}}"""
-//
-//    val scoredCardsOffers = Http().singleRequest(HttpRequest(method = HttpMethods.POST, uri = endpoint,
-//      entity = HttpEntity(ContentTypes.`application/json`, body))).flatMap {
-//      case HttpResponse(StatusCodes.OK, _, entity, _) =>
-//        entity.toStrict(5.seconds).map(strictEntity => JsonParser(ParserInput(strictEntity.data.utf8String)))
-//          .map(_.convertTo[ScoredCardsResponse])
-//
-//      case HttpResponse(status, _, entity, _) =>
-//        entity.toStrict(5.seconds).map(_.data.utf8String).flatMap(resultString => Future.failed(new IllegalStateException(
-//          s"Failed to get credit card offers from CSCards. URI: ${endpoint}, HTTP Status ${status}, entity: ${resultString}"
-//        )))
-//    }
-//
-//    //TODO: ensure this works and use Success/Failure cases for this
-//    scoredCardsOffers.onComplete(res => res)
-//    scoredCardsOffers
-//  }
+  def findCreditCardDeals(user: User, csCardsEndpoint: String, scoredCardsEndpoint: String) = {
+    for {
+      csCardOffers <- fetchCsCardOffers(user, csCardsEndpoint)
+      scoredCardOffers <- fetchScoredCardsOffers(user, scoredCardsEndpoint)
+    } yield {
+      (csCardOffers, scoredCardOffers)
+    }
+  }
+
+  def fetchCsCardOffers(user: User, endpoint: String): Future[List[CSCardsResponse]] = {
+    val body = s"""{"name": "${user.name}", "creditScore": ${user.creditScore}}"""
+
+    val csCardOffers = Http().singleRequest(
+      HttpRequest(
+        method = HttpMethods.POST,
+        uri = endpoint,
+        entity = HttpEntity(ContentTypes.`application/json`, body))).flatMap {
+      case HttpResponse(StatusCodes.OK, _, entity, _) => Unmarshal(entity).to[List[CSCardsResponse]]
+      case HttpResponse(status, _, entity, _) =>
+        entity.toStrict(5.seconds).map(_.data.utf8String).flatMap(resultString => Future.failed(
+          new IllegalStateException(
+            s"Failed to get credit card offers from CSCards. URI: ${endpoint}, HTTP Status ${status}, entity: ${resultString}")))
+    }
+
+    csCardOffers.onComplete(res => res)
+    csCardOffers
+  }
+
+  def fetchScoredCardsOffers(user: User, endpoint: String): Future[List[ScoredCardsResponse]] = {
+    val body = s"""{"name": "${user.name}", "score": ${user.creditScore}, "salary": ${user.salary}}"""
+
+    val scoredCardsOffers = Http().singleRequest(
+      HttpRequest(
+        method = HttpMethods.POST,
+        uri = endpoint,
+        entity = HttpEntity(ContentTypes.`application/json`, body))).flatMap {
+      case HttpResponse(StatusCodes.OK, _, entity, _) => Unmarshal(entity).to[List[ScoredCardsResponse]]
+      case HttpResponse(status, _, entity, _) =>
+        entity.toStrict(5.seconds).map(_.data.utf8String).flatMap(resultString => Future.failed(
+          new IllegalStateException(
+            s"Failed to get credit card offers from ScoredCards. URI: ${endpoint}, HTTP Status ${status}, entity: ${resultString}")))
+    }
+
+    scoredCardsOffers.onComplete(res => res)
+    scoredCardsOffers
+  }
 }
--- src/main/scala/com/clearscore/techtest/service/ServiceJsonProtocol.scala
+++ src/main/scala/com/clearscore/techtest/service/ServiceJsonProtocol.scala
@@ -2,13 +2,48 @@ package com.clearscore.techtest.service
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import com.clearscore.techtest.models._
-import spray.json.DefaultJsonProtocol
-import spray.json.DefaultJsonProtocol.iterableFormat
+import spray.json._
 
 trait ServiceJsonProtocol extends DefaultJsonProtocol with SprayJsonSupport {
   //JSON Serialization
   implicit val userFormat = jsonFormat3(User)
   implicit val serviceResponseFormat = jsonFormat4(CardQueryServiceResponse)
-  implicit val cscCardsResponseFormat = jsonFormat3(CSCardsResponse)
-  implicit val scoredCardsResponseFormat = jsonFormat3(ScoredCardsResponse)
+
+  //Ensure that CSCards response format can allow for JSON serialization and deserialization
+  implicit object cscCardsResponseFormat extends RootJsonFormat[CSCardsResponse] {
+
+    override def read(jsonValue: JsValue): CSCardsResponse = {
+      jsonValue.asJsObject.getFields("cardName", "apr", "eligibility") match {
+        case Seq(JsString(cardName), JsNumber(apr), JsNumber(eligibility)) =>
+           CSCardsResponse(cardName, apr.toDouble, eligibility.toDouble)
+
+        case _ => throw DeserializationException("Expected credit card from CSCards")
+      }
+    }
+
+    override def write(objValue: CSCardsResponse): JsObject = JsObject(
+      "cardName" -> JsString(objValue.cardName),
+      "apr" -> JsNumber(objValue.apr),
+      "eligibility" -> JsNumber(objValue.eligibility)
+    )
+  }
+
+  //Ensure that Scored Cards response format can allow for JSON serialization and deserialization
+  implicit object scoredCardsResponseFormat extends RootJsonFormat[ScoredCardsResponse] {
+
+    override def read(jsonValue: JsValue): ScoredCardsResponse = {
+      jsonValue.asJsObject.getFields("card", "apr", "approvalRating") match {
+        case Seq(JsString(card), JsNumber(apr), JsNumber(approvalRating)) =>
+          ScoredCardsResponse(card, apr.toDouble, approvalRating.toDouble)
+
+        case _ => throw new DeserializationException("Expected credit card from ScoredCards")
+      }
+    }
+
+    override def write(objValue: ScoredCardsResponse): JsObject = JsObject(
+      "card" -> JsString(objValue.card),
+      "apr" -> JsNumber(objValue.apr),
+      "approvalRating" -> JsNumber(objValue.approvalRating)
+    )
+  }
 }
